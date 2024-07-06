3dac1aac40f7a29565526ca68ea9973baa3598da
julie <julie.ryan.france@gmail.com>
2024-07-06 13:29:22 +0200

    first commit

---
+++ hangman/hangman/hangmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  hangmanApp.swift
+//  hangman
+//
+//  Created by julie ryan on 06/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct hangmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
